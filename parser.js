6ea4ae40c28e6d10ca8300a4e6d629ada25cbce9
Erick Sandoval <erickweeds@gmail.com>
2018-05-11 22:15:52 -0500

    Testing and comments

--- parser.js
+++ parser.js
@@ -1,14 +1,20 @@
 class  Parser{
-  constructor(jsonRes){
-    this.jsonRes = {};
+  constructor(){
+
   }
   parseXML(xmlSource){
-    var tag,tmp,closeTagPositon,openTag, value;
-    this.jsonRes = {};
+    var jsonRes = {};//JSON object
+    var tag,tmp,closeTagPositon,openTag, value;//helper variables
+    //cleaning the XML
     xmlSource = this.unescapeString(xmlSource);
+    //While XML string has <elemts>
     while(xmlSource.match(/<[^\/][^>]*>/)){
+      //getting the current open Tag
       openTag = xmlSource.match(/<[^\/][^>]*>/)[0];
+      //getting the tag name
       tag = openTag.substring(1, openTag.length - 1);
+      //looking for the </element> tag
+      //closeTagPosition will contain the index of the closing tag
       closeTagPositon = xmlSource.indexOf(openTag.replace('<', '</'));
       if(closeTagPositon == -1){
         tag = openTag.match(/[^<][\w+$]*/)[0];
@@ -17,27 +23,27 @@ class  Parser{
              closeTagPositon = xmlSource.indexOf('<\\/' + tag);
          }
       }
+      //getting the value between tags
       value = xmlSource.substring(openTag.length, closeTagPositon);
-
+      //if the inner value is a tag
        if (value.match(/<[^\/][^>]*>/)) {
+         //call to the function with the childrens nodes
            tmp = this.parseXML(value);
        }
        else {
-           tmp = value;
+           tmp = value; //if is a simple value
        }
-       if (this.jsonRes[tag] === undefined) {
-           this.jsonRes[tag] = tmp;
+       //if the object doesn't have the tag already
+       if (jsonRes[tag] === undefined) {
+           jsonRes[tag] = tmp; //creating the tag
        }
-       else if (Array.isArray(this.jsonRes[tag])) {
-           this.jsonRes[tag].push(tmp);
-       }
-       else {
-           this.jsonRes[tag] = [this.jsonRes[tag], tmp];
+       else if (Array.isArray(jsonRes[tag])) {//is there is a value with the same tag is an array
+           jsonRes[tag].push(tmp);
        }
 
        xmlSource = xmlSource.substring(openTag.length * 2 + 1 + value.length);
     }
-    return this.jsonRes;
+    return jsonRes;
   }
 
   unescapeString(string){
